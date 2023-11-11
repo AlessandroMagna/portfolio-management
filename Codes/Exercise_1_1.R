367d28c94781a0864255c6bae03e68c2283af33e
Magnabosco <Alessandro.Magnabosco@eneco.com>
2023-11-11 22:11:52 +0100

    Add first script

---
+++ Codes/Exercise_1_1.R
@@ -0,0 +1,5 @@
+setwd("C:/Users/510908/Documents/code/portfolio-management/Data")
+#import data F-F_Research_Data_Factors_daily
+data = read.csv("F-F_Research_Data_Factors_daily.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+View(data)
+
